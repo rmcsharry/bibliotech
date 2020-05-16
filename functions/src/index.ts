64f18670304da8bf7d27be07377a109a1c8b58ee
Richard McSharry <rmcsharry@gmail.com>
2020-05-16 22:20:58 +0200

    Comments out sendgrid function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
-import * as sgMail from '@sendgrid/mail'
+// import * as sgMail from '@sendgrid/mail'
 
 admin.initializeApp()
 
@@ -16,24 +16,24 @@ interface Message {
   message: string
 }
 
-sgMail.setApiKey(config.env.sendgrid.apiKey)
+// sgMail.setApiKey(config.env.sendgrid.apiKey)
 
-exports.sendEmail = functions.database.ref('messages/{pushId}/message').onCreate((snapshot, context) => {
-  const msg: Message = snapshot.val()
-  const mailOptions = {
-    from: `${msg.firstName} ${msg.lastName} <${msg.email}>`,
-    to: 'rmcsharry@gmail.com',
-    subject: 'Bibliotech - you have a new message',
-    html: `<p>Hi Juste,</p><br/><h4>You have a new message:</h4>
-       <h6> <b>Email: </b>${msg.email} </h6>
-       <h6> <b>First name: </b>${msg.firstName} </h6>
-       <h6> <b>Last name: </b>${msg.lastName} </h6>
-       <br/>
-       <p> <b>Message: </b>${msg.message} </p>`,
-  }
-  console.log(`Content is ${mailOptions}`)
-  return sgMail
-    .send(mailOptions)
-    .then(info => console.log('Email sent: ' + info))
-    .catch(error => console.log('Error sending email ---- ', error))
-})
+// exports.sendEmail = functions.database.ref('messages/{pushId}/message').onCreate((snapshot, context) => {
+//   const msg: Message = snapshot.val()
+//   const mailOptions = {
+//     from: `${msg.firstName} ${msg.lastName} <${msg.email}>`,
+//     to: 'rmcsharry@gmail.com',
+//     subject: 'Bibliotech - you have a new message',
+//     html: `<p>Hi Juste,</p><br/><h4>You have a new message:</h4>
+//        <h6> <b>Email: </b>${msg.email} </h6>
+//        <h6> <b>First name: </b>${msg.firstName} </h6>
+//        <h6> <b>Last name: </b>${msg.lastName} </h6>
+//        <br/>
+//        <p> <b>Message: </b>${msg.message} </p>`,
+//   }
+//   console.log(`Content is ${mailOptions}`)
+//   return sgMail
+//     .send(mailOptions)
+//     .then(info => console.log('Email sent: ' + info))
+//     .catch(error => console.log('Error sending email ---- ', error))
+// })

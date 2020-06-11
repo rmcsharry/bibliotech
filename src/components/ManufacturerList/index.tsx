9c63ea853d6acf970fed7ab35b772120e86d44c7
Richard McSharry <rmcsharry@gmail.com>
2020-06-11 13:01:08 +0200

    Fixes top margin on all pages

--- src/components/ManufacturerList/index.tsx
+++ src/components/ManufacturerList/index.tsx
@@ -89,7 +89,7 @@ const ManufacturerList: React.FC<IProps> = ({ isRestricted, favorites, onlyFavor
   }
 
   return (
-    <Container fluid style={{ marginTop: '140px' }}>
+    <Container fluid style={{ marginTop: '100px' }}>
       <Row className="justify-content-center mt-4">{list}</Row>
     </Container>
   )

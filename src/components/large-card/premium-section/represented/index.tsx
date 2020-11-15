3a3e02e2cfaa6fe0d73e45e76a3034e593554ca4
Richard McSharry <rmcsharry@gmail.com>
2020-11-16 00:05:08 +0100

    Fixes logo widths for represented logos

--- src/components/large-card/premium-section/represented/index.tsx
+++ src/components/large-card/premium-section/represented/index.tsx
@@ -14,7 +14,7 @@ const Represented: React.FC<IProps> = ({ companies, logos }) => {
         <a style={{ width: '150px' }} className="p-1" href={`https://www.bibliotech.ca/manufacturer/${item}`}>
           <img
             key={`feature-image-${index}`}
-            className="p-2 mb-1 w-25 mx-auto"
+            className="p-2 mb-1 w-100 mx-auto"
             src={logos[index].thumbnails.large.url}
             alt={`represented company logo ${index}`}
           ></img>
